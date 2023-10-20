b03c85811ebf04b56a76258c1f9e6a296c468645
Yuanuanuan <asdll6618@gmail.com>
2023-10-20 21:34:47 +0800

    feat: merge sort complete

--- app.js
+++ app.js
@@ -264,3 +264,144 @@ allTrash.forEach((btn) => {
     setGPA();
   })
 })
+
+// 排序 merge sort
+let btn1 = document.querySelector('.sort-descending')
+let btn2 = document.querySelector('.sort-ascending')
+btn1.addEventListener('click', () => {
+  handleSorting('descending');
+})
+btn2.addEventListener('click', () => {
+  handleSorting('ascending');
+})
+
+const handleSorting = (direction) => {
+  let graders = document.querySelectorAll('div.grader')
+  let objectArray = [];
+
+  for (let i = 0; i < graders.length; i++) {
+    let class_name = graders[i].children[0].value; // class category
+    let class_number = graders[i].children[1].value; // class number
+    let class_credit = graders[i].children[2].value;
+    let class_grade = graders[i].children[3].value;
+
+    if (
+      !(
+        class_name == "" &&
+        class_number == "" &&
+        class_credit == "" &&
+        class_grade == ""
+      )
+    ) {
+      let class_object = {
+        class_name,
+        class_number,
+        class_credit,
+        class_grade,
+      };
+      objectArray.push(class_object);
+    }
+  }
+
+  for (let i = 0; i < objectArray.length; i++) {
+    objectArray[i].class_grade_number = convertor(objectArray[i].class_grade);
+  }
+
+  objectArray = mergeSort(objectArray);
+  if (direction === 'descending') {
+    objectArray = objectArray.reverse();
+  }
+  
+  let allInputs = document.querySelector('.all-inputs');
+  allInputs.innerHTML = '';
+
+  for (let i = 0; i < objectArray.length; i++) {
+    allInputs.innerHTML += `
+    <form>
+      <div class="grader">
+          <input
+          type="text"
+          placeholder="class category"
+          class="class-type"
+          list="opt"
+          value=${objectArray[i].class_name}
+          /><!--
+          --><input
+          type="text"
+          placeholder="class number"
+          class="class-number"
+          value=${objectArray[i].class_number}
+          /><!--
+          --><input
+          type="number"
+          placeholder="credits"
+          min="0"
+          max="6"
+          class="class-credit"
+          value=${objectArray[i].class_credit}
+          /><!--
+          --><select name="select" class="select">
+          <option value=""></option>
+          <option value="A">A</option>
+          <option value="A-">A-</option>
+          <option value="B+">B+</option>
+          <option value="B">B</option>
+          <option value="B-">B-</option>
+          <option value="C+">C+</option>
+          <option value="C">C</option>
+          <option value="C-">C-</option>
+          <option value="D+">D+</option>
+          <option value="D">D</option>
+          <option value="D-">D-</option>
+          <option value="F">F</option></select
+          ><!--
+          --><button class="trash-button">
+          <i class="fas fa-trash"></i>
+          </button>
+      </div>
+    </form>
+    `;
+  }
+
+}
+
+const merge = (a1, a2) => {
+  let result = [];
+  let i = 0;
+  let j = 0;
+
+  while (i < a1.length && j < a2.length) {
+    if (a1[i].class_grade_number > a2[j].class_grade_number) {
+      result.push(a2[j]);
+      j++;
+    } else {
+      result.push(a1[i]);
+      i++;
+    }
+  }
+
+  while (i < a1.length) {
+    result.push(a1[i]);
+    i++;
+  }
+
+  while (j < a2.length) {
+    result.push(a2[j]);
+    j++;
+  }
+
+  return result;
+}
+
+const mergeSort = (arr) => {
+  if (arr.length === 0) return
+
+  if (arr.length === 1) {
+    return arr;
+  } else {
+    let middle = Math.floor(arr.length / 2);
+    let left = arr.slice(0, middle);
+    let right = arr.slice(middle, arr.length);
+    return merge(mergeSort(left), mergeSort(right));
+  }
+}
